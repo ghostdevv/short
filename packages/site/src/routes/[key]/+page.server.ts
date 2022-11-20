965e9f2b22b359a70b12c466d34f10bf1e366a8b
GHOST <ghostdevbusiness@gmail.com>
2022-11-20 20:39:43 +0000

    refactor: simplify

--- packages/site/src/routes/[key]/+page.server.ts
+++ packages/site/src/routes/[key]/+page.server.ts
@@ -1,26 +1,13 @@
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
-import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 import { fetch } from '$lib/fetch';
 import urlJoin from 'url-join';
 
-async function getRedirectUrl(key: string) {
-    try {
-        const data = await fetch<{ link: string }>(
-            urlJoin(PUBLIC_BACKEND_URL, '/get', key),
-        );
-
-        return data.link;
-    } catch (e) {
-        console.log('Error redirecting', e);
-
-        throw error(400, {
-            message: e as string,
-        });
-    }
-}
-
 export const load: PageServerLoad = async ({ params, cookies }) => {
-    const link = await getRedirectUrl(params.key);
+    const { link } = await fetch<{ link: string }>(
+        urlJoin(PUBLIC_BACKEND_URL, '/get', params.key),
+    );
+
     throw redirect(307, link);
 };
