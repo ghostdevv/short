9eb5d487d0e8da32fb79d6a16b2aa22fc8cff349
GHOST <ghostdevbusiness@gmail.com>
2022-11-20 05:42:33 +0000

    feat: redirect

---
+++ packages/site/src/routes/[key]/+page.server.ts
@@ -0,0 +1,32 @@
+import type { PageServerLoad } from './$types';
+import { error, redirect } from '@sveltejs/kit';
+import { fetch } from '$lib/fetch';
+import urlJoin from 'url-join';
+
+async function getRedirectUrl(backendUrl: string, key: string) {
+    try {
+        const data = await fetch<{ link: string }>(
+            urlJoin(backendUrl, '/get', key),
+        );
+
+        return data.link;
+    } catch (e) {
+        console.log('Error redirecting', e);
+        throw error(400, {
+            message: e as string,
+        });
+    }
+}
+
+export const load: PageServerLoad = async ({ params, cookies }) => {
+    const backendUrl = cookies.get('backendUrl');
+
+    if (typeof backendUrl != 'string')
+        throw error(400, {
+            message: 'Unable to find backend url',
+        });
+
+    const link = await getRedirectUrl(backendUrl, params.key);
+
+    throw redirect(307, link);
+};
---
+++ packages/site/src/routes/[key]/+page.svelte
@@ -0,0 +1,4 @@
+<section class="col">
+    <h1>You shouldn't see this</h1>
+    <a href="/">Return Home</a>
+</section>
