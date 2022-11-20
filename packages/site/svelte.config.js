120d586b887553854158dd986db5450f9b439701
GHOST <ghostdevbusiness@gmail.com>
2022-11-20 03:35:25 +0000

    fix: use ?

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -11,7 +11,7 @@ const config = {
         adapter: adapter(),
 
         csrf: {
-            checkOrigin: import.meta.env.MODE == 'dev',
+            checkOrigin: import.meta?.env?.MODE == 'dev',
         },
     },
 };
