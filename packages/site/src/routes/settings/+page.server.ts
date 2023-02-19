f7e54625dc9d562cbabf48a1b1b8414db169604d
GHOST <ghostdevbusiness@gmail.com>
2023-02-19 23:48:07 +0000

    refactor: use zod

--- packages/site/src/routes/settings/+page.server.ts
+++ packages/site/src/routes/settings/+page.server.ts
@@ -1,20 +1,20 @@
 import type { Actions } from './$types';
 import { fail } from '@sveltejs/kit';
+import { z } from 'zod';
 
 export const actions: Actions = {
     general: async ({ request, cookies }) => {
         const data = await request.formData();
-
         const account = data.get('account');
 
         if (typeof account == 'string') {
-            const uuidRegex =
-                /^(?:[0-9A-Za-z]{8}-[0-9A-Za-z]{4}-4[0-9A-Za-z]{3}-[89ABab][0-9A-Za-z]{3}-[0-9A-Za-z]{12})/;
+            const uuid = z.string().uuid();
+            const result = uuid.safeParse(account);
 
-            if (!account.match(uuidRegex))
+            if (!result.success)
                 return fail(400, {
-                    account,
                     error: 'The account UUID must be a valid UUID',
+                    account,
                 });
 
             cookies.set('account', account, {
