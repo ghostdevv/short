54d73a4315e2202144521895aac9a27ade835dac
GHOST <ghostdevbusiness@gmail.com>
2023-02-19 22:39:52 +0000

    chore: add kv namespaces

--- packages/site/src/hooks.server.ts
+++ packages/site/src/hooks.server.ts
@@ -1,4 +1,7 @@
+import { MemoryStorage } from '@miniflare/storage-memory';
+import { KVNamespace as KV } from '@miniflare/kv';
 import type { Handle } from '@sveltejs/kit';
+import { dev } from '$app/environment';
 import { fetch } from '$lib/fetch';
 
 async function getAccount(potential?: string) {
@@ -13,11 +16,20 @@ async function getAccount(potential?: string) {
     return potential;
 }
 
+function kv() {
+    return new KV(new MemoryStorage()) as KVNamespace;
+}
+
 export const handle: Handle = async ({ event, resolve }) => {
     const account = await getAccount(event.cookies.get('account'));
 
     event.locals.account = account;
     event.cookies.set('account', account);
+
+    if (dev) {
+        event.platform.env.LINKS = kv();
+        event.platform.env.LINKS_MAP = kv();
+    }
 
     return resolve(event);
 };
