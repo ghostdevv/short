93551d385e78c435a70102d3cbeadb5b1a327fad
GHOST <ghostdevbusiness@gmail.com>
2023-02-20 00:23:33 +0000

    refactor: convert to non monorepo

--- packages/api/src/utils/route.ts
+++
@@ -1,4 +0,0 @@
-import type { Context } from '../types';
-import { Handler } from 'worktop';
-
-export const route = (handler: Handler<Context>) => handler;
--- packages/api/src/utils/zod.ts
+++
@@ -1,5 +0,0 @@
-import type { ZodIssue } from 'zod';
-
-export function formatZodIssue(issue: ZodIssue) {
-    return `[${issue.path.join(', ')}] ${issue.message}`;
-}
