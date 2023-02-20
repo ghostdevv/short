8d7ce0281eef7900e42e821fff48b2150e7d1024
GHOST <ghostdevbusiness@gmail.com>
2023-02-20 01:26:01 +0000

    chore: update colour thing

--- src/lib/colour.ts
+++ src/lib/colour.ts
@@ -1,29 +1,22 @@
-// modified from https://stackoverflow.com/a/13532993/16207260 - ty
-export function lightenColour(colour: string, percent: number) {
-    let R = parseInt(colour.substring(1, 3), 16);
-    let G = parseInt(colour.substring(3, 5), 16);
-    let B = parseInt(colour.substring(5, 7), 16);
+// gotta love chatgpt
+export function lightenHex(hex: string, amount: number): string {
+    // Remove the hash character if it's present
+    hex = hex.replace('#', '');
 
-    R = (R * (100 + percent)) / 100;
-    G = (G * (100 + percent)) / 100;
-    B = (B * (100 + percent)) / 100;
+    // Convert the hex color to an RGB color
+    const r = parseInt(hex.substr(0, 2), 16);
+    const g = parseInt(hex.substr(2, 2), 16);
+    const b = parseInt(hex.substr(4, 2), 16);
 
-    R = R < 255 ? R : 255;
-    G = G < 255 ? G : 255;
-    B = B < 255 ? B : 255;
+    // Calculate the new, lighter RGB color
+    const newR = Math.min(Math.round(r + (255 - r) * amount), 255);
+    const newG = Math.min(Math.round(g + (255 - g) * amount), 255);
+    const newB = Math.min(Math.round(b + (255 - b) * amount), 255);
 
-    R = Math.round(R);
-    G = Math.round(G);
-    B = Math.round(B);
+    // Convert the new RGB color back to hex format
+    const newHex = `#${newR.toString(16).padStart(2, '0')}${newG
+        .toString(16)
+        .padStart(2, '0')}${newB.toString(16).padStart(2, '0')}`;
 
-    const RR =
-        R.toString(16).length == 1 ? `0${R.toString(16)}` : R.toString(16);
-
-    const GG =
-        G.toString(16).length == 1 ? `0${G.toString(16)}` : G.toString(16);
-
-    const BB =
-        B.toString(16).length == 1 ? `0${B.toString(16)}` : B.toString(16);
-
-    return `#${RR}${GG}${BB}`;
+    return newHex;
 }
