02d394b021f5195ab64bf552d4b2789bbe75e43c
GHOST <ghostdevbusiness@gmail.com>
2024-02-10 23:29:08 +0000

    refactor: createLink function

--- src/lib/server/links.ts
+++ src/lib/server/links.ts
@@ -1,4 +1,6 @@
-import type { Link } from '$lib/types';
+import { resolveExpiry, type ExpiryString } from '$lib/expiry';
+import { LinkType, type Link } from '$lib/types';
+import { nanoid } from 'nanoid';
 
 async function deleteLink(platform: App.Platform, link: Link) {
     console.log('deleting', link.key);
@@ -8,15 +10,45 @@ async function deleteLink(platform: App.Platform, link: Link) {
 }
 
 export async function getLink(platform: App.Platform, key: string) {
-    const raw = await platform.env.LINKS.get(key);
-    if (!raw) return null;
-
-    const link: Link = JSON.parse(raw);
+    const link = await platform.env.LINKS.get<Link>(key, 'json');
+    if (!link) return null;
 
     if (link.expiresAt <= Date.now()) {
         await deleteLink(platform, link);
         return null;
     }
+
+    return link;
+}
+
+async function generateKey(LINKS: KVNamespace): Promise<string> {
+    const key = nanoid(8).toLowerCase();
+    const exists = await LINKS.get(key);
+
+    return exists ? await generateKey(LINKS) : key;
+}
+
+type CreateLinkOptions = Omit<Link, 'key' | 'expiresAt' | 'type'> & {
+    expiry: ExpiryString;
+};
+
+export async function createLink(
+    platform: App.Platform,
+    options: CreateLinkOptions,
+) {
+    const expiresAt = resolveExpiry(options.expiry);
+    const key = await generateKey(platform.env.LINKS);
+
+    const link: Link = {
+        account: options.account,
+        link: options.link,
+        type: LinkType.Basic,
+        expiresAt,
+        key,
+    };
+
+    await platform.env.LINKS.put(key, JSON.stringify(link));
+    await platform.env.LINKS_MAP.put(`${options.account}:${key}`, '');
 
     return link;
 }
