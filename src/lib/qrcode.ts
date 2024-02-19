f881e417064609fdecf66ea2ae6bd7e2666e0a1b
GHOST <ghostdevbusiness@gmail.com>
2024-02-18 19:37:39 -0500

    feat: add qr code image option
    Closes #27

--- src/lib/qrcode.ts
+++ src/lib/qrcode.ts
@@ -1,10 +1,22 @@
 import type { Action } from 'svelte/action';
+import { qrCodeImage } from './settings';
 import type { KjuaOptions } from 'kjua';
+import { get } from 'svelte/store';
 import type Kjua from 'kjua';
 
 export let options: KjuaOptions;
 
 export const qrcode: Action<HTMLDivElement, KjuaOptions> = (node, options) => {
+    // const imageSrc = get(qrCodeImage);
+
+    // if (imageSrc) {
+    //     const image = document.createElement('img');
+    //     image.style.display = 'none';
+    //     image.src = imageSrc;
+
+    //     options = { ...options, image, mode: 'image' };
+    // }
+
     let codeElement: HTMLElement | undefined;
     let kjua: typeof Kjua;
 
